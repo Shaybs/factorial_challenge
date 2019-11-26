5f12fe3db38e98fd0ecdf8491849c7b10d789c6e
Shaybs <ucashus@ucl.ac.uk>
2019-11-26 16:09:31 +0000

    Using floats

--- inverse_factorial/inverse_factorial.py
+++ inverse_factorial/inverse_factorial.py
@@ -1,26 +1,24 @@
 
 def inverse_factorial(number):
     factorial_test = number
-    divisor = 2
-    count = 1
+    divisor = 2.0
+    count = 1.0
     
-    while number > 1:
+    while number > 1.0:
         number = number / divisor
-        divisor = divisor + 1
-        count += 1
+        divisor = divisor + 1.0
+        count += 1.0
     
-    if number == 1:
-        if count > 1:
+    if number == 1.0:
+        if count > 1.0:
             a = "{0}!".format(count)
             print(a)
             return a
-        elif count == 1:
+        elif count == 1.0:
             c = "{0}! or 0!".format(count)
             print(c)
             return c
-    elif number != 1:
+    elif number != 1.0:
         b = "NONE"
         print(b)
         return b
-    
-    #hi
--- inverse_factorial_test/inverse_factorial_test.py
+++ inverse_factorial_test/inverse_factorial_test.py
@@ -2,13 +2,13 @@ import pytest
 from inverse_factorial import inverse_factorial
 
 def test_inverse_factorial():
-    assert inverse_factorial.inverse_factorial(120) == '5!'
+    assert inverse_factorial.inverse_factorial(120) == '5.0!'
 
 def test_inverse_factorial_2():
     assert inverse_factorial.inverse_factorial(150) == 'NONE'
 
 def test_inverse_factorial_3():
-    assert inverse_factorial.inverse_factorial(6) == '3!'
+    assert inverse_factorial.inverse_factorial(6) == '3.0!'
 
 def test_inverse_factorial_4():
     assert inverse_factorial.inverse_factorial(10) == 'NONE'
@@ -20,7 +20,7 @@ def test_inverse_factorial_6():
     assert inverse_factorial.inverse_factorial(-2) == 'NONE'
 
 def test_inverse_factorial_7():
-    assert inverse_factorial.inverse_factorial(1) == '1! or 0!'
+    assert inverse_factorial.inverse_factorial(1) == '1.0! or 0!'
 
 def test_inverse_factorial_8():
     assert inverse_factorial.inverse_factorial(1000000) == 'NONE'
@@ -29,7 +29,7 @@ def test_inverse_factorial_9():
     assert inverse_factorial.inverse_factorial(-3) == 'NONE'
 
 def test_inverse_factorial_10():
-    assert inverse_factorial.inverse_factorial(2) == '2!'
+    assert inverse_factorial.inverse_factorial(2) == '2.0!'
 
 def test_inverse_factorial_11():
     assert inverse_factorial.inverse_factorial(3) == 'NONE'
